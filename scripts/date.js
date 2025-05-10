2002d58d8abc77c76769ef109586e237c9513d24
kelwood22 <kwoodland@byupathway.edu>
2025-05-10 17:42:33 -0600

    Course Home Page Initial

---
+++ scripts/date.js
@@ -0,0 +1,8 @@
+
+
+
+const currentYear = new Date().getFullYear();
+const lastModified = new Date(document.lastModified);
+
+document.getElementById('currentYear').textContent = currentYear;
+document.getElementById('lastModified').textContent = lastModified;
---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+
+
+const menuButton = document.getElementById('menu');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+});
